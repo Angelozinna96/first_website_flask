bc2c4272d4f140a18a2117f2c5dec43f9291fad9
Unknown <angelozinna96@gmail.com>
2019-11-30 16:30:52 +0000

    first implementation login

--- venv/app/models.py
+++ venv/app/models.py
@@ -7,17 +7,28 @@ Created on Thu Nov 28 17:38:30 2019
 """
 
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-class User(db.Model):
+from flask_login import UserMixin
+from app import login
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(64), index=True, unique=True)
     lastname = db.Column(db.String(64), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    def set_passwd(self,passwd):
+        self.password_hash= generate_password_hash(passwd)
+    def check_passwd(self,passwd):
+        return check_password_hash(self.password_hash,passwd)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name =  db.Column(db.String(64),index=True)
