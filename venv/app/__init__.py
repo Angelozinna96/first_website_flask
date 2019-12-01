0bcce5af1f74e362ebba4c50b4bef1a12bf5a646
Unknown <angelozinna96@gmail.com>
2019-12-01 13:17:12 +0000

    creating user handler

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -19,3 +19,5 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+login = LoginManager(app)
+login.login_view = 'login'
--- venv/app/routes.py
+++ venv/app/routes.py
@@ -6,8 +6,12 @@ Created on Sat Nov 16 15:28:33 2019
 @author: angelozinna
 """
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user,logout_user, login_required
+from app.models import User
 from app import app
 from app.forms import LoginForm, SignupForm, CreateEventForm
+from flask import request
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
@@ -27,12 +31,24 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index')) 
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-            form.username.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_passwd(form.password.data):
+            flash('Login failed : invalid username or password! ')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
@@ -42,6 +58,7 @@ def signup():
         return redirect(url_for('index'))
     return render_template('signup.html', title='Sign Up', form=form)
 @app.route('/create_event', methods=['GET', 'POST'])
+@login_required
 def createevent():
     form = CreateEventForm()
     if form.validate_on_submit():
